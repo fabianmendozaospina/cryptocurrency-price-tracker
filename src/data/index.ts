b3c8d8e22b8090e001fbd1726a172d1d06abb80a
Fabian <fabianmendozaospina@outlook.com>
2024-12-22 14:29:03 -0600

    Initial commit

---
+++ src/data/index.ts
@@ -0,0 +1,8 @@
+import { Currency } from "../types";
+
+export const currencies : Currency[] = [
+  { code: 'USD', name: 'United States Dollar'},
+  { code: 'COP', name: 'Colombian Peso'},
+  { code: 'EUR', name: 'Euro'},
+  { code: 'GBP', name: 'Pound Sterling'},
+]
